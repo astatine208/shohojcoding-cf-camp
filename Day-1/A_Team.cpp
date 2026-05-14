a320479bae568c812041864807072dca5ee77c01
Kamruzzaman Tuhin <153776215+astatine208@users.noreply.github.com>
2026-05-15 02:20:32 +0600

    Solve: A_Temp
    Initial solution for Day 1.

---
+++ Day-1/A_Team.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    int count=0;
+    while(t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b+c>=2)
+            count++;
+    }
+    cout<<count;
+    return 0;
+}
