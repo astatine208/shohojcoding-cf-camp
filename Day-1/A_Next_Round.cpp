ca25c910babedfb0773264deef317dfeea8c0514
Kamruzzaman Tuhin <153776215+astatine208@users.noreply.github.com>
2026-08-10 23:12:29 +0600

    Add Day-1 problem solutions.

---
+++ Day-1/A_Next_Round.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,k,x,count=0;
+    cin>>n>>k;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        x=arr[k-1];
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]>=x && arr[i]>0)    count++;
+    }
+    cout<<count;
+    return 0;
+}
